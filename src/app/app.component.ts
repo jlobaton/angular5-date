e15ee4300de13131a93ccd3bfb5c2fcdea7da08a
Jesus Lobaton - Laptop <jesuslobaton@gmail.com>
2017-12-05 11:52:55 -0400

    Codigo Funcional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DatepickerOptions } from 'ng2-datepicker';
+import * as frLocale from 'date-fns/locale/es';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  date = new Date();
+
+optionsDate: DatepickerOptions = {
+  minYear: this.date.getFullYear(),
+  maxYear: 2030,
+  displayFormat: 'DD-MM-YYYY',
+  barTitleFormat: 'MMMM YYYY',
+  firstCalendarDay: 1, // 0 - Sunday, 1 - Monday
+  locale: frLocale
+};  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgDatepickerModule } from 'ng2-datepicker';
 import { AppComponent } from './app.component';
 
 
@@ -10,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+  NgbModule.forRoot(),
+    BrowserModule,
+    NgDatepickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
